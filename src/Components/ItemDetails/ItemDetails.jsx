39928584f2dfb8425118dcf2120eb098b7e86410
YoussefMagedMohamed <joeyoussef307@gmail.com>
2023-05-03 22:34:56 +0300

    item Details

--- src/Components/ItemDetails/ItemDetails.jsx
+++ src/Components/ItemDetails/ItemDetails.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import "../ItemDetails/ItemDetails.css"
+import "../ItemDetails/ItemDetails.css";
 
 const ItemDetails = () => {
   let { id } = useParams();
@@ -21,15 +21,17 @@ const ItemDetails = () => {
     getItemDetails(id);
   }, []);
 
-  // let { strMealThumb } = details.meals
-
   return details.map((value, index) => (
     <div key={index} data={value} className="container mt-5">
       <div className="row">
         <div className="col-md-5">
-          <img src={details[0].strMealThumb} className="w-100" alt="Meal Name" />
+          <img
+            src={details[0].strMealThumb}
+            className="w-100"
+            alt="Meal Name"
+          />
         </div>
-        <div className="col-md-7">
+        <div className="col-md-7 meal-main">
           <h1>{details[0].strMeal}</h1>
           <div className="categoryName">
             <h1>Category :</h1>
@@ -39,9 +41,11 @@ const ItemDetails = () => {
             <h1>Area :</h1>
             <h1>{details[0].strArea}</h1>
           </div>
-          <div className="overview">
-            <p>{details[0].strInstructions}</p>
-          </div>
+        </div>
+      </div>
+      <div className="row my-5">
+        <div className="overview text-white">
+          <p>{details[0].strInstructions}</p>
         </div>
       </div>
     </div>
